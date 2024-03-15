5989958bbdc8bd1b788e03a4a789366cd6ab5b9c
mooizahmed102 <amoiz6263@gmail.com>
2024-03-15 23:54:04 +0500

    first

--- views/home.php
+++ views/home.php
@@ -827,6 +827,7 @@ Dr. J.K Hudson
                     <div class="col-md-4">
                         <div class="copy-txt">
                             <p style="font-size:12px; color:#fff;">Copyright © 2024 Amazon Publishing Spot  . All Rights Reserved. </p>
+                        <p style="font-size:14px; color:#fff;">Disclaimer: Saibina Corp</p>
                         </div>
                     </div>
                     <div class="col-md-4"></div>
