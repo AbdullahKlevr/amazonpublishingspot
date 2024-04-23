0264995e700d1a1efa7a10e335ab320bf5389b54
mooizahmed102 <amoiz6263@gmail.com>
2024-04-24 01:07:42 +0500

    first commit

--- views/services-rates-writing.php
+++ views/services-rates-writing.php
@@ -149,7 +149,7 @@
             <div class="container-fluid">
 
                 <a class="navbar-brand" href="/">
-                    <img class="logo horizontal-logo" src="images/blacklogo.png" alt="Brand Logo">
+                    <img class="logo horizontal-logo" src="images/blacklogo.png" alt="Brand Logo" style="max-width:50%;">
                 </a>
                 <div class="collapse navbar-collapse topnavs">
                     <ul class="navbar-nav ml-auto">
